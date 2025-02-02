76e25a84be2295fa25bc1ad12037d641d6cd3fb2
Jon Evans <jon@rezendi.com>
2025-02-01 22:17:04 -0800

    closer to writing sloprank reports

--- missions/management/commands/sloprank_report.py
+++ missions/management/commands/sloprank_report.py
@@ -1,16 +1,18 @@
 from django.core.management.base import BaseCommand
-from missions.models import MissionInfo
-from missions.hub import fulfil_mission
-from missions.admin_jobs import get_random_repo
-from missions.util import log
-
 from django.db.models import CharField
 from django.db.models.functions import Lower
+
+from missions.admin_jobs import get_random_repo
+from missions.hub import fulfil_mission
+from missions.models import MissionInfo
+from missions.prompts import get_prompt_from_github
+from missions.util import log
 
 CharField.register_lookup(Lower, "lower")
 
 
 SLOPRANK_MISSION = "SlopRank Repo Analysis"
+
 
 class Command(BaseCommand):
     help = "Grab a random repo from GitHub's Daily Trending and run a report on it"
@@ -26,11 +28,28 @@ class Command(BaseCommand):
         mission_info = MissionInfo.objects.get(name=SLOPRANK_MISSION)
         mission = mission_info.create_mission()
         if options["llm"]:
-            mission.llm = options["llm"] # by default we use OpenAI if not set
+            mission.llm = options["llm"]  # by default we use OpenAI if not set
         mission.flags["github"] = selected
         mission.save()
 
         fulfil_mission(mission.id)
         log("SlopRank report complete")
 
-        # TODO append report to CSV file for SlopRank
+        log("Writing to CSV")
+        final_risk_report = mission.task_set.filter(name="Final Risk Report").first()
+        prompt = get_prompt_from_github("risk-analysis")
+
+        rows = []  # TODO gete from file arg
+        rows.append(
+            {
+                "prompt": prompt,
+                "model": final_risk_report.get_llm(),
+                "response": final_risk_report.response,
+                "is_valid": True,
+                "response_time": "todo",
+                "Answer_key": "n/a",
+                "token_count": "todo",
+                "error": None,
+            }
+        )
+        # write rows to same CSV file
